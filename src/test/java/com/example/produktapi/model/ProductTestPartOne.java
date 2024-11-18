265fb500ea8cb44caf6af021b0de5e6dd5e8b6f0
BarnaliD <115993238+BarnaliD@users.noreply.github.com>
2024-11-18 16:22:09 +0100

    Update unit test for setPrice method

--- src/test/java/com/example/produktapi/model/ProductTestPartOne.java
+++ src/test/java/com/example/produktapi/model/ProductTestPartOne.java
@@ -12,11 +12,17 @@ class ProductTestPartOne {
     }
 
     @Test
+    //Barnali Test
     void setPrice() {
         Product product = new Product();
-        product.setPrice(199.99);
-        assertEquals(199.99, product.getPrice());
+
+        // Act: Set the price
+        product.setPrice(299.99);
+
+        // Assert: Ensure the price is set correctly
+        assertEquals(299.99, product.getPrice());
     }
+
 
     @Test
     void getCategory() {
