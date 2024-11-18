c2a3316c3c65a02c34681919db018e89dfc08c90
ib222dp <ib222dp@student.lnu.se>
2024-11-18 13:22:36 +0100

    feature/1_unittest_Product - added tests

---
+++ src/test/java/com/example/produktapi/model/ProductTest_Product.java
@@ -0,0 +1,43 @@
+package com.example.produktapi.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTestProduct {
+
+    @Test
+    public void test_Product_empty_constructor() {
+        // Act
+        Product product = new Product();
+
+        // Assert
+        assertNotNull(product);
+        assertNull(product.getTitle());
+        assertNull(product.getPrice());
+        assertNull(product.getCategory());
+        assertNull(product.getDescription());
+        assertNull(product.getImage());
+    }
+
+    @Test
+    public void test_Product(){
+        // Setup
+        String productName = "product1";
+        Double price = 5.99;
+        String category = "cat1";
+        String description = "desc1";
+        String image = "img1";
+
+        // Act
+        Product product = new Product(productName, price, category, description, image);
+
+        // Assert
+        assertEquals(productName, product.getTitle());
+        assertEquals(price, product.getPrice());
+        assertEquals(category, product.getCategory());
+        assertEquals(description, product.getDescription());
+        assertEquals(image, product.getImage());
+    }
+
+}
